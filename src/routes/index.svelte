0062921f4d2270017a0eb3ee4b8b428fde0bf4ae
Erinc Argimak <erar93@gmail.com>
2022-03-15 22:07:01 +0100

    Move index styling into the component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,13 @@
 <h1>sketchbook.erinc.space</h1>
+
+<p>The cover page will be here eventually, and the sketches will follow.</p>
+
+<style scss>
+	h1 {
+		@apply text-sky-700 text-7xl;
+	}
+
+	p {
+		@apply text-sky-900 text-2xl;
+	}
+</style>
